8d3173445f87a7c85cd9773bb92f786c3808df2f
Huang Jihao <705626108@qq.com>
2024-01-02 16:51:25 +0800

    添加知乎热榜

--- command_invoker.py
+++ command_invoker.py
@@ -1,6 +1,7 @@
 from command.gpt_reply import reply_by_gpt35, reply_by_gpt4
 from command.help import get_help_msg
 from command.bili_hot import get_bili_hot_str
+from command.zhihu_hot import get_zhihu_hot_str
 from send_msg import send_text_msg, send_file_msg, send_image_msg
 
 
@@ -33,7 +34,7 @@ class CommandInvoker:
     # 命令：/zhihu-hot
     @staticmethod
     def cmd_zhihu_hot(to_user_name: str) -> None:
-        send_text_msg("get_zhihu_hot_str()", to_user_name)
+        send_text_msg(get_zhihu_hot_str(), to_user_name)
 
     # 命令：/weibo-hot
     @staticmethod
