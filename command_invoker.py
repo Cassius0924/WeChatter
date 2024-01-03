a28943c388b77ef14b646cd1c15e301567a85daa
Huang Jihao <705626108@qq.com>
2024-01-03 17:44:11 +0800

    添加github-trending

--- command_invoker.py
+++ command_invoker.py
@@ -4,7 +4,7 @@ from command.bili_hot import get_bili_hot_str
 from command.zhihu_hot import get_zhihu_hot_str
 from command.weibo_hot import get_weibo_hot_str
 from command.transalte import get_reverso_context_tran_str, detect_lang, check_lang_support
-from command.github_trending import get_github_trending_str as get_github_trending_str
+from command.github_trending import get_github_trending_str
 from send_msg import send_text_msg, send_file_msg, send_image_msg
 
 
