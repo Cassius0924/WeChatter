52fecfd17989e481783f5fe335eff9504f36fecc
Huang Jihao <705626108@qq.com>
2024-01-03 19:47:15 +0800

    优化github-trending

--- command/github_trending.py
+++ command/github_trending.py
@@ -8,7 +8,7 @@ def get_github_trending_str() -> str:
 
     trending_str = "✨=====GitHub Trending=====✨\n"
     for i, trending in enumerate(trending_list[:10]):  # 只获取前10个趋势
-        trending_str += f"{i + 1}. {trending['author']} / {trending['repo']}\n"
+        trending_str += f"{i + 1}. {trending['author']} / {trending['repo']} {trending['svg']}\n"
     return trending_str
 
 def get_github_trending_list() -> list:
@@ -25,12 +25,19 @@ def get_github_trending_list() -> list:
             if title:
                 repo = title['href'].split('/')
                 if len(repo) >= 3:
-                    trending_list.append({
-                        'author': repo[1].strip(),
-                        'repo': repo[2].strip()
-                    })
+                    svg = article.select_one('svg') # 提取SVG内容
+                    if svg:
+                        svg_content = svg.prettify() # 获取SVG的完整内容
+                        trending_list.append({
+                            'author': repo[1].strip(),
+                            'repo': repo[2].strip(),
+                            'svg': svg_content
+                        })
 
         return trending_list[:20]
 
     print("获取GitHub趋势失败")
     return []
+
+# 调用函数并打印结果
+print(get_github_trending_str())
