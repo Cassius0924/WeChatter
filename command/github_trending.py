5590973607b957ab78fc076a1a132d4a1088ccdb
Huang Jihao <705626108@qq.com>
2024-01-03 22:51:28 +0800

    优化github-trending

--- command/github_trending.py
+++ command/github_trending.py
@@ -18,7 +18,11 @@ def get_github_trending_str() -> str:
 
 def get_github_trending_list() -> list:
     url = "https://github.com/trending"
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
