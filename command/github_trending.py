855853874ed05b814bb2af1c660f6e75b23b244f
Huang Jihao <705626108@qq.com>
2024-01-04 10:54:34 +0800

    优化github-trending

--- command/github_trending.py
+++ command/github_trending.py
@@ -9,7 +9,7 @@ def get_github_trending_str() -> str:
 
     trending_str = "✨=====GitHub Trending=====✨\n"
     for i, trending in enumerate(trending_list[:10]):  # 只获取前10个趋势
-        trending_str += f"{i + 1}. {trending['author']} / {trending['repo']}\n  ⭐ {trending['star_total']}total(⭐{trending['star_today']})\n  🔤 {trending['programmingLanguage']}\n"  # 📖 {trending['comment']}\n"
+        trending_str += f"{i + 1}. {trending['author']} / {trending['repo']}\n  ⭐ {trending['star_total']}total(⭐{trending['star_today']})\n  🔤 {trending['programmingLanguage']}\n  📖 {trending['comment']}\n"
     return trending_str
 
 
@@ -35,6 +35,12 @@ def get_github_trending_list() -> list:
             # comment = article.select_one("p").get_text(strip=True)
             # if comment:
             #     trending_item["comment"] = comment
+
+            comment_elem = article.select_one("p")
+            if comment_elem:
+                comment = comment_elem.get_text(strip=True)
+                trending_item["comment"] = comment
+
 
             programming_language = article.select_one(
                 "span[itemprop='programmingLanguage']"
