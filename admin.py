b448215244c02dfcd97bb03f5aad4cbe5b99a8c6
Cassius0924 <2670226747@qq.com>
2024-01-05 19:56:18 +0800

    fix: 修复admin发送错误

--- admin.py
+++ admin.py
@@ -5,9 +5,9 @@ from config import admin_list
 
 # 给所有管理发消息
 # TODO: 合并到Sender类中
-def send_msg_to_all_admin(messsage: str) -> None:
+def send_msg_to_all_admin(message: str) -> None:
     if len(admin_list) == 0:
         print("管理员列表为空")
         return
     for admin in admin_list:
-        Sender.send_text_msg_p(messsage, admin)
+        Sender.send_text_msg_p(admin, message)
