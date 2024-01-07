a771fc0931761b98b00cb6f434ab4bf74d877152
Huang Jihao <705626108@qq.com>
2024-01-07 23:01:59 +0800

    feat:增加todo的功能

--- message_parser.py
+++ message_parser.py
@@ -18,6 +18,8 @@ class MessageParser:
         # cmd = message.cmd # 命令
         desc = message.cmd_desc  # 命令描述
         cmd_value = message.cmd_value  # 命令值
+        id = message.source.p_info.id  # 发送者id
+        name = message.source.p_info.name  # 发送者昵称
 
         print(desc)
         # 非命令消息
@@ -71,6 +73,19 @@ class MessageParser:
 
         elif cmd_value == self.__get_cmd_value("today"):
             CommandInvoker.cmd_today_in_history(to)
+
+        elif cmd_value == self.__get_cmd_value("pai-post"):
+            CommandInvoker.cmd_pai_post(to)
+
+        elif cmd_value == self.__get_cmd_value("today"):
+            CommandInvoker.cmd_today_in_history(to)
+
+        elif cmd_value == self.__get_cmd_value("todo"):
+            CommandInvoker.cmd_todo(to, msg, id, name)
+
+        elif cmd_value == self.__get_cmd_value("rmtd"):
+            CommandInvoker.cmd_remove_todo(to, msg, id, name)
+
 
     # 获取命令值
     def __get_cmd_value(self, cmd: str) -> int:
