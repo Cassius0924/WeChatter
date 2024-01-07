06a988ea60a73d27f6651200d5ee7c3a16a4aea6
Cassius0924 <2670226747@qq.com>
2024-01-07 16:07:55 +0800

    feat: 配置文件添加管理员群

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 # 管理员管理
-from send_msg import Sender
+from send_msg import SendMessage, SendMessageType, Sender
 from main import cr
 
 
@@ -9,5 +9,9 @@ def send_msg_to_all_admin(message: str) -> None:
     if len(cr.admin_list) == 0:
         print("管理员列表为空")
         return
-    for admin in cr.admin_list:
-        Sender.send_text_msg_p(admin, message)
+    Sender.send_msg_ps(cr.admin_list, SendMessage(SendMessageType.TEXT, message))
+    if len(cr.admin_group_list) == 0:
+        print("管理员群列表为空")
+        return
+    Sender.send_msg_gs(cr.admin_group_list, SendMessage(SendMessageType.TEXT, message))
+
