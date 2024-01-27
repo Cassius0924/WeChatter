3f4e162a38b7c1ccfa22071fd2e804b47f73a6b0
Huang Jihao <705626108@qq.com>
2024-01-27 22:33:21 +0800

    feat:新增询问食物卡路里功能

--- wechatter/commands/_commands/food_calories.py
+++ wechatter/commands/_commands/food_calories.py
@@ -24,7 +24,7 @@ def get_food_str(message: str) -> str:
     if food_list == []:
         return "获取食物列表失败"
     food_str = "✨=====食物列表=====✨\n"
-    for i, food in enumerate(food_list[:5]):
+    for i, food in enumerate(food_list[:10]):
         food_id = food.get('foodId')
         food_details = get_food_details(food_id)
         food_str += f"{i + 1}.  {food.get('name')}\n{food_details}\n\n"
