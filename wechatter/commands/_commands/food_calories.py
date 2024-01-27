6f135a8e28438e4040412c7f0a0ba4de885b2b54
Huang Jihao <705626108@qq.com>
2024-01-27 21:53:37 +0800

    feat:新增询问食物卡路里功能

--- wechatter/commands/_commands/food_calories.py
+++ wechatter/commands/_commands/food_calories.py
@@ -24,8 +24,10 @@ def get_food_str(message: str) -> str:
     if food_list == []:
         return "获取食物列表失败"
     food_str = "✨=====食物列表=====✨\n"
-    for i, food in enumerate(food_list[:20]):
-        food_str += f"{i + 1}.  {food.get('name')}      {str(food.get('calory')).rjust(10)}大卡\n"
+    for i, food in enumerate(food_list[:5]):
+        food_id = food.get('foodId')
+        food_details = get_food_details(food_id)
+        food_str += f"{i + 1}.  {food.get('name')}    热量:{str(food.get('calory')).rjust(10)}大卡\n{food_details}\n\n"
     return food_str
 
 
@@ -39,3 +41,20 @@ def get_food_list(message: str) -> List:
         return []
     food_list = response.json()
     return food_list.get("data", {}).get("list", [])
+
+
+def get_food_details(food_id: str) -> str:
+    if food_id == []:
+        return "获取食物id失败"
+
+    url = (
+        f"https://www.mxnzp.com/api/food_heat/food/details?foodId={food_id}&app_id=pprqn2hfekkrr8k8&app_secret=nVFcuE6htNjhtGIkJURrS2qQm8L3HUCR")
+    response = requests.get(url, timeout=10)
+
+    if response.status_code != 200:
+        print("获取食物详情失败")
+        return []
+    food_details = response.json()
+    food = food_details.get("data", {})
+    food_str = f"热量:{food.get('calory')}卡路里    蛋白质:{food.get('protein')}g\n脂肪:{food.get('fat')}g    碳水化合物:{food.get('carbohydrate')}g\n钠值:{food.get('natrium')}mg    锌值:{food.get('zinc')}mg\n铁值:{food.get('iron')}mg    钙值:{food.get('calcium')}mg\ngi值:{food.get('glycemicInfoData').get('gi').get('value')}({food.get('glycemicInfoData').get('gi').get('label')})    gl值:{food.get('glycemicInfoData').get('gl').get('value')}({food.get('glycemicInfoData').get('gi').get('label')})\n{food.get('healthTips')}({food.get('healthSuggest')})"
+    return food_str
