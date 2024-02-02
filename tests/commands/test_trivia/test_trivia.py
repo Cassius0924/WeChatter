ab650c508b91c2f242731f2daa2b3f2861f01289
Huang Jihao <705626108@qq.com>
2024-02-03 01:14:15 +0800

    feat:添加trivia的测试代码

--- tests/commands/test_trivia/test_trivia.py
+++ tests/commands/test_trivia/test_trivia.py
@@ -15,14 +15,14 @@ class TestTriviaCommand(unittest.TestCase):
         with open('tests/commands/test_trivia/trivia_data.json') as f:
             self.trivia_list = json.load(f)
 
-    # def test_parse_trivia_response_success(self):
-    #     result = trivia._parse_trivia_response(self.response)
-    #     self.assertListEqual(result, self.trivia_list)
+    def test_parse_trivia_response_success(self):
+        result = trivia._parse_trivia_response(self.response)
+        self.assertListEqual(result, self.trivia_list)
 
     # def test_parse_trivia_response_failure(self):
     #     with self.assertRaises(Bs4ParsingError):
     #         trivia._parse_trivia_response(Response())
-
+    #
     # def test_generate_trivia_message_success(self):
     #     result = trivia._generate_trivia_message(self.trivia_list, 1)
     #     true_result = ""
