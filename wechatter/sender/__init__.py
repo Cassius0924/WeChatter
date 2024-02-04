746a036dd5b7de1ed92ed73155428a95713ea754
Cassius0924 <2670226747@qq.com>
2024-02-04 12:14:13 +0800

    feat: 优化收到命令提醒

--- wechatter/sender/__init__.py
+++ wechatter/sender/__init__.py
@@ -1,3 +1,4 @@
+from .notifier import notify_logged_in, notify_logged_out, notify_received
 from .sender import Sender
 
-__all__ = ["Sender"]
+__all__ = ["Sender", "notify_received", "notify_logged_in", "notify_logged_out"]
