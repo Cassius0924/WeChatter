d9a58ce54b30a0966ffbf6c4c80d21d69c41b94f
Cassius0924 <2670226747@qq.com>
2024-02-04 12:22:10 +0800

    fix: 修复循环导入

--- wechatter/sender/__init__.py
+++ wechatter/sender/__init__.py
@@ -1,4 +1,6 @@
-from .notifier import notify_logged_in, notify_logged_out, notify_received
+# isort: off
 from .sender import Sender
+from .notifier import notify_logged_in, notify_logged_out, notify_received
+# isort: on
 
 __all__ = ["Sender", "notify_received", "notify_logged_in", "notify_logged_out"]
