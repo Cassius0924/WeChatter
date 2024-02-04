fe1b13bc41fcda1e8c1926014fb997c6b9575e2b
Huang Jihao <705626108@qq.com>
2024-02-04 23:36:41 +0800

    feat:test:获取中国石油化工92号汽油指导价

--- wechatter/commands/_commands/food_calories.py
+++ wechatter/commands/_commands/food_calories.py
@@ -21,7 +21,7 @@ def food_calories_command_handler(to: SendTo, message: str = "") -> None:
         response = get_request(
             url=f"https://www.boohee.com/food/search?keyword={message}"
         )
-        print(response.text)
+        print(response)
         food_href_list = parse_food_href_list_response(response)
         food_detail_list = get_food_detail_list(food_href_list)
         result = generate_food_message(food_detail_list)
