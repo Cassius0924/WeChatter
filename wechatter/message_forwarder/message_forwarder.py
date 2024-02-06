9486d5f3faff8297d49a01bd11cc7d01a5df6c85
Huang Jihao <705626108@qq.com>
2024-02-06 16:58:53 +0800

    feat:消息转发支持所有人*

--- wechatter/message_forwarder/message_forwarder.py
+++ wechatter/message_forwarder/message_forwarder.py
@@ -27,8 +27,8 @@ class MessageForwarder:
         # 判断消息是否符合转发规则
         for rule in self.rule_list:
             # 如果发送者列表为*，则代表发送者为所有人
-            if rule["froms"] is "*":
-                rule["froms"] = [from_name]
+            # if rule["froms"] is "*":
+            #     rule["froms"] = [from_name]
             # # 除去在接收者列表中发送的消息
             # if from_name in rule["to_persons"]:
             #     continue
