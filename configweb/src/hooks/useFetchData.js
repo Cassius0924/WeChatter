5d584989629809d616b465ea1686c05e02491353
Huang Jihao <705626108@qq.com>
2024-02-13 00:41:12 +0800

    增加ConfigWeb

--- configweb/src/hooks/useFetchData.js
+++ configweb/src/hooks/useFetchData.js
@@ -7,7 +7,7 @@ function useFetchData(path) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/${path}`)
+        axios.get(`http://127.0.01:8000/${path}`)
         // axios.get(`http://47.92.99.199:30/api/${path}`)
         // axios.get(`172.24.191.12:8000/${path}`)
             .then(res => {
