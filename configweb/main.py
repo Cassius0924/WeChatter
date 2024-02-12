8f256bf36b393cec78e29cf711c17642b3e2bde8
Huang Jihao <705626108@qq.com>
2024-02-13 01:46:31 +0800

    增加ConfigWeb

--- configweb/main.py
+++ configweb/main.py
@@ -8,12 +8,17 @@ from fastapi.middleware.cors import CORSMiddleware
 logging.basicConfig(level=logging.DEBUG)
 app = FastAPI()
 
+BASE_URL = "http://47.92.99.199:"
+PORT = "3000"
+
+# 其他代码...
+
 origins = [
 #    "http://localhost:8000",
 #    "http://localhost:3000",
-    "http://47.92.99.199:3000",
 #    "http://47.92.99.199:30",
 #    "http://47.92.99.199:8080"
+        BASE_URL + PORT,
 ]
 
 app.add_middleware(
--- configweb/src/hooks/useFetchData.js
+++ configweb/src/hooks/useFetchData.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+export const BASE_URL = "http://47.92.99.199:";
+export const PORT = "8000";
 function useFetchData(path) {
     const [data, setData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
@@ -9,7 +11,7 @@ function useFetchData(path) {
     useEffect(() => {
         // axios.get(`http://127.0.0.1:8000/${path}`)
         // axios.get(`http://47.92.99.199:30/api/${path}`)
-        axios.get(`http://47.92.99.199:8000/${path}`)
+        axios.get(`${BASE_URL}${PORT}/${path}`)
             .then(res => {
                 setData(res.data);
                 setIsLoading(false);
