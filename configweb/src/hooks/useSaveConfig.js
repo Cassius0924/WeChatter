5876f6aa8f49d19f35960afecbc915e3abeeb491
Huang Jihao <705626108@qq.com>
2024-02-13 00:21:55 +0800

    增加ConfigWeb

--- configweb/src/hooks/useSaveConfig.js
+++ configweb/src/hooks/useSaveConfig.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 function useSaveConfig(configName, config) {
     return useCallback(() => {
-        // axios.post(`http://127.0.0.1:8000/${configName}`, config)
-        axios.post(`http://47.92.99.199:30/api/${configName}`, config)
+        axios.post(`http://127.0.0.1:8000/${configName}`, config)
+        // axios.post(`http://47.92.99.199:30/api/${configName}`, config)
             .then(res => {
                 console.log(res.data);
                 // 可以在这里添加一些处理，例如显示一个通知告诉用户配置已经保存成功
