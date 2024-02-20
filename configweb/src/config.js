69a0a02a91ebb0447d58f85b917ca4f9562e8825
Huang Jihao <705626108@qq.com>
2024-02-20 17:17:24 +0800

    增加ConfigWeb

--- configweb/src/config.js
+++ configweb/src/config.js
@@ -4,5 +4,6 @@
 // export const BASE_URL = "localhost";
 // export const PORT = "8000";
 //服务器
-export const BASE_URL = "47.92.99.199";
+// export const BASE_URL = "47.92.99.199";
+export const BASE_URL = "0.0.0.0";
 export const PORT = "8000";
