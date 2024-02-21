d9880eeec0098c2f9d484d5214c3f0cca45bff2e
Huang Jihao <705626108@qq.com>
2024-02-22 01:26:39 +0800

    配置文件从ini变成yaml格式

--- configweb/src/ui/WeChatter.js
+++ configweb/src/ui/WeChatter.js
@@ -4,8 +4,8 @@ import useFetchData from '../hooks/useFetchData';
 import useSaveConfig from '../hooks/useSaveConfig';
 
 function WeChatter() {
-    const [config, setConfig] = useFetchData('wechatter_port');
-    const handleSave = useSaveConfig('wechatter_port', config);
+    const [config, setConfig] = useFetchData('wechatter');
+    const handleSave = useSaveConfig('wechatter', config);
 
     return (
         <div className="border-4 border-dashed border-gray-200 rounded-lg mb-6">
