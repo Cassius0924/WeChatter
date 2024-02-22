18e1cd4d84335ded787b84f0863432fea34117f1
Huang Jihao <705626108@qq.com>
2024-02-22 16:32:41 +0800

    配置文件从ini变成yaml格式

--- configweb/src/ui/Chat.js
+++ configweb/src/ui/Chat.js
@@ -31,7 +31,7 @@ function Chat() {
                     <input type="text"
                            className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm"
                            placeholder="True"
-                           value={config.need_mentioned || ''}
+                           value={config.need_mentioned.toString() || ''}
                            onChange={e => setConfig({...config, need_mentioned: e.target.value})}/>
                     <button
                         onClick={handleSave}
