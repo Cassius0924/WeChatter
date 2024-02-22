736b1b4c7a246231dd636521a27a17f74e0c0940
Huang Jihao <705626108@qq.com>
2024-02-22 20:28:37 +0800

    配置文件从ini变成yaml格式

--- configweb/main.py
+++ configweb/main.py
@@ -174,7 +174,8 @@ def get_wx_bot_webhook_config():
 def update_wx_bot_webhook_config(updated_config: dict = Body(...)):
     succeed_wx_webhook_base_api = update_config_section('wx_webhook_base_api', updated_config)
     succeed_wx_webhook_recv_api_path = update_config_section('wx_webhook_recv_api_path', updated_config)
-    return succeed_wx_webhook_base_api and succeed_wx_webhook_recv_api_path
+    reslut = {**succeed_wx_webhook_base_api, **succeed_wx_webhook_recv_api_path}
+    return reslut
 
 
 
