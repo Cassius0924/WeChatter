25bafdb05e4d444595efce0d88a9e0402f3a4cf2
Huang Jihao <705626108@qq.com>
2024-02-23 05:24:31 +0800

    配置文件从ini变成yaml格式

--- configweb/src/ui/TaskCron.js
+++ configweb/src/ui/TaskCron.js
@@ -74,9 +74,12 @@ function TaskCron() {
         setTaskCronList(newTaskCronList);
     };
 
-    useEffect(() => {//解释：这里的useEffect是为了监听taskCronList的变化，一旦taskCronList变化，就会触发这个useEffect
-        setConfig({...config, task_cron_list: taskCronList});
-    }, [taskCronList]);
+    // useEffect(() => {//解释：这里的useEffect是为了监听taskCronList的变化，一旦taskCronList变化，就会触发这个useEffect
+    //     setConfig({...config, task_cron_list: taskCronList});
+    // }, [taskCronList]);
+    useEffect(() => {
+        setTaskCronList(config.task_cron_list || []);
+    }, [config]);
 
     return (
         <div className="border-4 border-dashed border-gray-200 rounded-lg mb-6">
