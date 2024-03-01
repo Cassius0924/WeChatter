2cd3d7a63f89fb91e68a22744c644c0a458977fa
Huang Jihao <705626108@qq.com>
2024-03-02 00:28:57 +0800

    把configweb的前后端ip和端口的配置信息加到config.yaml

--- configweb/src/config.js
+++ configweb/src/config.js
@@ -11,49 +11,15 @@
 // export const BACKEND_PORT = jsonConfig.backend_port;
 
 //config.js
-import React, {useEffect} from 'react';
+// config.js
 import useFetchData from './hooks/useFetchData';
 
-function get_BACKEND_IP_and_BACKEND_PORT() {
-    const [config, setConfig] = useFetchData('backend');
+async function get_BACKEND_IP_and_BACKEND_PORT() {
+    const config = await useFetchData('backend');
     const BACKEND_IP = config.backend_ip;
     const BACKEND_PORT = config.backend_port;
-    useEffect(() => {
-        console.log('config changed');
-        console.log(config);
-    }, [config]);
 
-    return (
-        <div className="border-4 border-dashed border-gray-200 rounded-lg mb-6">
-            <div className="flex flex-col items-center justify-center h-full">
-                <div className="text-center">
-                    <h3 className="mb-4 text-lg leading-6 font-medium text-gray-900">
-                        backend_ip
-                    </h3>
-                    <p className="mb-4 text-sm leading-5 text-gray-500">
-                        后端IP地址
-                    </p>
-                    <input type="text"
-                           className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm"
-                           placeholder="localhost"
-                           value={config.backend_ip || ''}
-                           onChange={e => setConfig({...config, backend_ip: e.target.value})}/>
-                    <h3 className="mb-4 text-lg leading-6 font-medium text-gray-900">
-                        backend_port
-                    </h3>
-                    <p className="mb-4 text-sm leading-5 text-gray-500">
-                        后端端口
-                    </p>
-                    <input type="text"
-                           className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm"
-                           placeholder="8000"
-                           value={config.backend_port || ''}
-                           onChange={e => setConfig({...config, backend_port: e.target.value})}/>
-                </div>
-            </div>
-        </div>
-    )
+    return [BACKEND_IP, BACKEND_PORT];
 }
 
-// export default get_BACKEND_IP_and_BACKEND_PORT;
-export const [BACKEND_IP, BACKEND_PORT] = get_BACKEND_IP_and_BACKEND_PORT()
+export const [BACKEND_IP, BACKEND_PORT] = get_BACKEND_IP_and_BACKEND_PORT();
