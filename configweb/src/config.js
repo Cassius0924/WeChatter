61c4b9ef5cc471106b59a65b79501f4bac4057c7
Huang Jihao <705626108@qq.com>
2024-03-02 00:30:44 +0800

    把configweb的前后端ip和端口的配置信息加到config.yaml

--- configweb/src/config.js
+++ configweb/src/config.js
@@ -4,22 +4,5 @@
 // export const BACKEND_IP = "localhost";
 // export const BACKEND_PORT = "8000";
 //服务器
-// export const BACKEND_IP = "47.92.99.199";
-// export const BACKEND_PORT = "8000";
-
-// export const BACKEND_IP = jsonConfig.backend_ip;
-// export const BACKEND_PORT = jsonConfig.backend_port;
-
-//config.js
-// config.js
-import useFetchData from './hooks/useFetchData';
-
-async function get_BACKEND_IP_and_BACKEND_PORT() {
-    const config = await useFetchData('backend');
-    const BACKEND_IP = config.backend_ip;
-    const BACKEND_PORT = config.backend_port;
-
-    return [BACKEND_IP, BACKEND_PORT];
-}
-
-export const [BACKEND_IP, BACKEND_PORT] = get_BACKEND_IP_and_BACKEND_PORT();
+export const BACKEND_IP = "47.92.99.199";
+export const BACKEND_PORT = "8000";
