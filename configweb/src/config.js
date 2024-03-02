468eb5e04930c409ab3cbb651cdae48411aa4eb2
Huang Jihao <705626108@qq.com>
2024-03-02 18:44:48 +0800

    把configweb的前后端ip和端口的配置信息加到config.yaml

--- configweb/src/config.js
+++ configweb/src/config.js
@@ -1,13 +1,9 @@
 //后端配置
 
-//本地
-// export const BACKEND_IP = "localhost";
-// export const BACKEND_PORT = "8000";
+// 本地
+export const BACKEND_IP = "localhost";
+export const BACKEND_PORT = "8000";
 
 // 服务器
 // export const BACKEND_IP = "your_ip";
 // export const BACKEND_PORT = "8000";
-
-
-export const BACKEND_IP = process.env.REACT_APP_BACKEND_IP;
-export const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT;
