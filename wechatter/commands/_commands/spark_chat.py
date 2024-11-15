aa72ef1930e99b13294345a27ed70f0966027c1b
Jerryestt <705626108@qq.com>
2024-11-15 14:41:02 +0800

    feat:星火认知大模型Spark

--- wechatter/commands/_commands/spark_chat.py
+++ wechatter/commands/_commands/spark_chat.py
@@ -393,7 +393,7 @@ class SparkChat:
         print(r_json)
         # 判断是否有 error 或 code 字段
         if r_json:
-            SparkChat._check_r_json(this_model, r_json)
+            SparkChat._check_r_json(r_json)
 
         msg = r_json["choices"][0]["message"]
         msg_content = msg.get("content", "调用" + this_model + "服务失败")
@@ -414,22 +414,22 @@ class SparkChat:
         return msg_content
 
     @staticmethod
-    def _check_r_json(cls, r_json):
+    def _check_r_json(r_json):
         if "error" in r_json:
             raise ValueError(this_model + " 服务返回值错误")
         if "code" in r_json:
             code = r_json["code"]
             error_messages = {
                 0: None,  # 成功，不抛出异常
-                10007: cls.this_model + " 用户流量受限：服务正在处理用户当前的问题，需等待处理完成后再发送新的请求。（必须要等大模型完全回复之后，才能发送下一个问题）",
-                10013: cls.this_model + " 输入内容审核不通过，涉嫌违规，请重新调整输入内容",
-                10014: cls.this_model + " 输出内容涉及敏感信息，审核不通过，后续结果无法展示给用户",
-                10019: cls.this_model + " 表示本次会话内容有涉及违规信息的倾向；建议开发者收到此错误码后给用户一个输入涉及违规的提示",
-                10907: cls.this_model + " token数量超过上限。对话历史+问题的字数太多，需要精简输入",
-                11200: cls.this_model + " 授权错误：该appId没有相关功能的授权 或者 业务量超过限制",
-                11201: cls.this_model + " 授权错误：日流控超限。超过当日最大访问量的限制",
-                11202: cls.this_model + " 授权错误：秒级流控超限。秒级并发超过授权路数限制",
-                11203: cls.this_model + " 授权错误：并发流控超限。并发路数超过授权路数限制",
+                10007: this_model + " 用户流量受限：服务正在处理用户当前的问题，需等待处理完成后再发送新的请求。（必须要等大模型完全回复之后，才能发送下一个问题）",
+                10013: this_model + " 输入内容审核不通过，涉嫌违规，请重新调整输入内容",
+                10014: this_model + " 输出内容涉及敏感信息，审核不通过，后续结果无法展示给用户",
+                10019: this_model + " 表示本次会话内容有涉及违规信息的倾向；建议开发者收到此错误码后给用户一个输入涉及违规的提示",
+                10907: this_model + " token数量超过上限。对话历史+问题的字数太多，需要精简输入",
+                11200: this_model + " 授权错误：该appId没有相关功能的授权 或者 业务量超过限制",
+                11201: this_model + " 授权错误：日流控超限。超过当日最大访问量的限制",
+                11202: this_model + " 授权错误：秒级流控超限。秒级并发超过授权路数限制",
+                11203: this_model + " 授权错误：并发流控超限。并发路数超过授权路数限制",
             }
             error_message = error_messages.get(code)
             if error_message:
