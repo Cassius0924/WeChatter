cdfce5f01ad7a1ba7f35f7a71ffda532da16f332
Jerryestt <705626108@qq.com>
2024-12-04 21:13:22 +0800

    fix: 优化消息转发规则解析逻辑
    - 重构了消息转发规则的解析逻辑，提高了代码的可读性和可维护性- 新增了将目标群组和人员自动添加到排除列表的功能，避免消息循环转发

--- wechatter/config/parsers/message_forwarding_rule_list_parser.py
+++ wechatter/config/parsers/message_forwarding_rule_list_parser.py
@@ -20,8 +20,13 @@ def parse_message_forwarding_rule_list(rule_list: List) -> Tuple[Dict, Dict]:
             all_message_rule["from_list_exclude"].extend(
                 rule.get("from_list_exclude", [])
             )
-            all_message_rule["to_group_list"].extend(rule.get("to_group_list", []))
-            all_message_rule["to_person_list"].extend(rule.get("to_person_list", []))
+            to_group_list = rule.get("to_group_list", [])
+            to_person_list = rule.get("to_person_list", [])            
+            all_message_rule["to_group_list"].extend(to_group_list)
+            all_message_rule["to_person_list"].extend(to_person_list)
+            
+            all_message_rule["from_list_exclude"].extend(to_group_list)
+            all_message_rule["from_list_exclude"].extend(to_person_list)
         else:
             for from_name in rule["from_list"]:
                 if from_name not in specific_message_rules:
